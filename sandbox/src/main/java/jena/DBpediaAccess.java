2ec23ab3c0db98402947e961622e284efab21b8f
tn00gm <tn00gm@gmail.com>
2014-03-24 20:34:04 +0900

    query to DBpedia

--- sandbox/src/main/java/jena/DBpediaAccess.java
+++ sandbox/src/main/java/jena/DBpediaAccess.java
@@ -1,9 +1,22 @@
 package jena;
 
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.ResultSet;
+
 public class DBpediaAccess {
 	
 	public static void main(String[] args) {
+		String service = "http://dbpedia.org/sparql";
+		String query = "select * where { ?x ?y ?z . } limit 100";
+		QueryExecution qe = QueryExecutionFactory.sparqlService(service, query);
+		
+		for ( ResultSet s = qe.execSelect(); s.hasNext(); ) {
+			System.out.println(s.next());
+		}
+
 		System.out.println(">> end of process <<");
+
 	}
 	
 }
